2edc4f68642967022f5121279a7d4d01c07f98b2
kimberlynolsin <kimberly.nolsin@gmail.com>
2023-09-25 11:35:51 -0400

    add form to signup page

--- src/pages/Signup/SignUp.jsx
+++ src/pages/Signup/SignUp.jsx
@@ -7,19 +7,19 @@ const SignUp = () => {
       <section className="signup">
         <h1 className="signup__title">Create A New Account</h1>
         <form className="signup__form">
-          <div>
-            <label htmlFor="name">Name:</label>
+          <div className="signup__form__containers">
+            <label className="signup__form__labels" htmlFor="name">Name:</label>
             <input type="text" id="name" name="name" />
           </div>
-          <div>
-            <label htmlFor="email">Email:</label>
+          <div className="signup__form__containers">
+            <label className="signup__form__labels" htmlFor="email">Email:</label>
             <input type="email" id="email" name="email" />
           </div>
-          <div>
-            <label htmlFor="password">Password:</label>
+          <div className="signup__form__containers">
+            <label className="signup__form__labels" htmlFor="password">Password:</label>
             <input type="password" id="password" name="password" />
           </div>
-          <button type="submit">Sign Up</button>
+          <button type="submit">Enter</button>
         </form>
       </section>
     </>
