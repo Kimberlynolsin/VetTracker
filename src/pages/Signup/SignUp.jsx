a8cc0ab4d4ea28f62f3eda93a640fc49e366317b
kimberlynolsin <kimberly.nolsin@gmail.com>
2023-09-26 11:28:52 -0400

    fixed form styling

--- src/pages/Signup/SignUp.jsx
+++ src/pages/Signup/SignUp.jsx
@@ -8,18 +8,26 @@ const SignUp = () => {
         <h1 className="signup__title">Create A New Account</h1>
         <form className="signup__form">
           <div className="signup__form__containers">
-            <label className="signup__form__labels" htmlFor="name">Name:</label>
+            <label className="signup__form__labels" htmlFor="name">
+              Name:
+            </label>
             <input type="text" id="name" name="name" />
           </div>
           <div className="signup__form__containers">
-            <label className="signup__form__labels" htmlFor="email">Email:</label>
+            <label className="signup__form__labels" htmlFor="email">
+              Email:
+            </label>
             <input type="email" id="email" name="email" />
           </div>
           <div className="signup__form__containers">
-            <label className="signup__form__labels" htmlFor="password">Password:</label>
+            <label className="signup__form__labels" htmlFor="password">
+              Password:
+            </label>
             <input type="password" id="password" name="password" />
           </div>
-          <button type="submit">Enter</button>
+          <div className="signup__form__containers signup__form__containers--btns">
+            <button type="submit">Enter</button>
+          </div>
         </form>
       </section>
     </>
