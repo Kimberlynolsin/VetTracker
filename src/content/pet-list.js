fc36b4be60ecb6508ab0f9761b9cc81f2c559266
Kimberly Nolsin <106711032+Kimberlynolsin@users.noreply.github.com>
2023-10-11 22:53:22 -0400

    add mock data (#10)

--- src/content/pet-list.js
+++ src/content/pet-list.js
@@ -1,4 +1,3 @@
-import more from "../assets/icons/more.png";
 export const petList = [
   {
     id: 1,
@@ -7,8 +6,12 @@ export const petList = [
     appointment: "12/01/2023",
     lastAppointment: "10/01/2023",
     note: "Brush your long-haired cat weekly to prevent matting.",
-    vaccine: "Rabbies"
-    
+    vaccine: [
+      { id: 1, date: "01/01/22", type: "Rabbies" },
+      { id: 2, date: "01/01/22", type: "Feline Viral Rhinotracheitis (FVRCP)" },
+      { id: 3, date: "01/01/22", type: "Panleukopenia (FPV)"},
+      { id: 4, date: "01/01/22", type: "Feline Calicivirus (FCV)" },
+    ],
   },
   {
     id: 2,
@@ -17,8 +20,12 @@ export const petList = [
     appointment: "12/01/2023",
     lastAppointment: "10/01/2023",
     note: "Feed 1 cup of high-quality dry kibble twice daily.",
-    vaccine: "Rabbies"
-    
+    vaccine: [
+      { id: 1, date: "01/01/22", type: "Rabbies" },
+      { id: 2, date: "01/01/22", type: "Feline Viral Rhinotracheitis (FVRCP)" },
+      { id: 3, date: "01/01/22", type: "Panleukopenia (FPV)"},
+      { id: 4, date: "01/01/22", type: "Feline Calicivirus (FCV)" },
+    ],
   },
   {
     id: 3,
@@ -27,8 +34,12 @@ export const petList = [
     appointment: "12/01/2023",
     lastAppointment: "10/01/2023",
     note: "Follow-up appointments every 3 months for monitoring.",
-    vaccine: "Rabbies"
-    
+    vaccine: [
+      { id: 1, date: "01/01/22", type: "Rabbies" },
+      { id: 2, date: "01/01/22", type: "Feline Viral Rhinotracheitis (FVRCP)" },
+      { id: 3, date: "01/01/22", type: "Panleukopenia (FPV)"},
+      { id: 4, date: "01/01/22", type: "Feline Calicivirus (FCV)" },
+    ],
   },
   {
     id: 4,
@@ -37,7 +48,11 @@ export const petList = [
     appointment: "12/01/2023",
     lastAppointment: "10/01/2023",
     note: "This dog exhibits signs of separation anxiety.",
-    vaccine: "Rabbies"
-    
+    vaccine: [
+      { id:1 ,date: "01/01/22", type: "Rabbies"},
+      { id:2 ,date: "01/01/22", type: "Feline Viral Rhinotracheitis (FVRCP)"},
+      { id:3 ,date: "01/01/22", type: "Panleukopenia (FPV)"},
+      { id:4 ,date: "01/01/22", type: "Feline Calicivirus (FCV)"},
+    ],
   },
 ];
