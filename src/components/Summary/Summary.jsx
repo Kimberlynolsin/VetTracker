66860350ea48299a988aa7253748785ce1ee2335
kimberlynolsin <kimberly.nolsin@gmail.com>
2023-10-11 22:52:17 -0400

    add mock data

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -7,20 +7,38 @@ const Summary = ({ selectedPet }) => {
           <div className="summary__container summary__container--name">
             <h3 className="summary__subtitle"> {selectedPet.name}</h3>
             <div className="summary__details">
-              <p className="summary__text">{selectedPet.type}</p>
+              <p className="summary__text">
+                {`Your pet ${selectedPet.name} is a ${selectedPet.type}!`}{" "}
+              </p>
             </div>
           </div>
           <div className="summary__container summary__container--vaccine">
-            <h3 className="summary__subtitle"> Vaccine History</h3>
+            <h3 className="summary__subtitle">Vaccine History</h3>
             <div className="summary__details">
-              <p className="summary__text">{selectedPet.vaccine}</p>
+              {selectedPet.vaccine.map((x) => {
+                return (
+                  <table key={x.id}>
+                    <thead>
+                      <tr>
+                        <th className="summary__date">{x.date}</th>
+                        <th className="summary__type">{x.type}</th>
+                      </tr>
+                    </thead>
+                  </table>
+                );
+              })}
             </div>
           </div>
           <div className="summary__container summary__container--appointment">
             <h3 className="summary__subtitle"> Appointments</h3>
             <div className="summary__details">
-              <p className="summary__text"><strong>Next: </strong>{selectedPet.appointment}</p>
-              <p className="summary__text"><strong>Last: </strong> {selectedPet.lastAppointment}</p>
+              <p className="summary__text">
+                <strong>Next: </strong>
+                {selectedPet.appointment}
+              </p>
+              <p className="summary__text">
+                <strong>Last: </strong> {selectedPet.lastAppointment}
+              </p>
             </div>
           </div>
           <div className="summary__container summary__container--notes">
